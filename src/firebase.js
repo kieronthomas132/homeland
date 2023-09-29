781a57a47c25b72f2b0e0f37e8ab98e3d47c6243
kieronthomas <kieron.thomas321@hotmail.co.uk>
2023-09-29 20:01:11 +0100

    updated env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDJ7lJGfN5O-ZBoZIEPscQ_GpYqXYu80HU",
+  authDomain: "real-estate-b2a3c.firebaseapp.com",
+  projectId: "real-estate-b2a3c",
+  storageBucket: "real-estate-b2a3c.appspot.com",
+  messagingSenderId: "769470851813",
+  appId: "1:769470851813:web:024bc5d6863e41c3a9020c",
+  measurementId: "G-1293GLJE9W"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
+
+console.log(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET)
